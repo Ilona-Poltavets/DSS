f5d689bc4063803946726c903a68bfa6253abc2b
Ilona-Poltavets <20ilona.poltavets02@gmail.com>
2021-06-04 19:56:17 +0300

    migrate

---
+++ database/migrations/2021_06_04_120316_create_reasons_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReasonsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reasons', function (Blueprint $table) {
+            $table->id()->unique();
+            $table->integer('alarm_id')->default(null);
+            $table->string('name');
+            $table->integer('expert_opinion');
+            $table->integer('experts_count');
+            $table->integer('priority');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reasons');
+    }
+}
